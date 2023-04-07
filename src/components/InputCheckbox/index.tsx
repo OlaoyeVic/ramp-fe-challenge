d433df8859210aa326c2d08451a2557c06c3e6f7
OlaoyeVic <olaoyevictor451@gmail.com>
2023-04-07 11:16:30 +0100

    fix: refactor onchange callback

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -1,9 +1,26 @@
 import classNames from "classnames"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { InputCheckboxComponent } from "./types"
 
 export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const [checkedBox, setCheckedBox] = useState(checked)
   const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+
+  const handleCheckBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = event.target.checked
+    setCheckedBox(checked)
+    onChange(checked)
+  }
+
+  // const handleCheckBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const isChecked = event.target.checked
+
+  //   if (isChecked) {
+  //     return checked
+  //   } else {
+  //     return !checked
+  //   }
+  // }
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
@@ -19,7 +36,7 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         className="RampInputCheckbox--input"
         checked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        onChange={handleCheckBoxChange}
       />
     </div>
   )
